c197e6c5bd2b861fcd2210626f67bd818570df81
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-17 16:05:34 +0900

    회원가입 기초세팅

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,18 +2,18 @@ let index = {
 	
 	init: function() {
 		$("#btn-save").bind("click", () => {
-			alert("btn-save 버튼이 눌러졌습니다");
+			this.save();
 		});
 	},
 	
-	save: function() {
+	save: function()  {
 		let data = {
-			username: $("#username").val(),
-			password: $("#password").val(),
+			username:$("#username").val(),
+			password:$("#password").val(),
 			email: $("#email").val()
 		}
-	}
-	
+	console.log(data);
+	}	
 }
 
 index.init();

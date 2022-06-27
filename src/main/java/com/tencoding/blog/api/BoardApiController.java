d9210eb2b0135c15ab1ec41c4b2da25b14074ef1
devWonil <102580743+devWonil@users.noreply.github.com>
2022-06-27 11:47:32 +0900

    글쓰기 완료

---
+++ src/main/java/com/tencoding/blog/api/BoardApiController.java
@@ -0,0 +1,34 @@
+package com.tencoding.blog.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tencoding.blog.auth.PrincipalDetail;
+import com.tencoding.blog.dto.ResponseDto;
+import com.tencoding.blog.model.Board;
+import com.tencoding.blog.service.BoardService;
+
+@RestController
+public class BoardApiController {
+
+	@Autowired
+	BoardService boardService;
+	
+	// 문제 인식 !!
+	// 세션을 어떻게 가져와야하는가
+	// @AuthenticationPrincipal  PrincipalDetail detail
+	// 1. 주소 맵핑, 데이터 받기
+	@PostMapping("/api/board")
+	public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal  PrincipalDetail detail) {
+		
+		boardService.write(board, detail.getUser());
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
+	// 2. 서비스 레이어 만들기
+}
--- src/main/java/com/tencoding/blog/api/UserApiController.java
+++ src/main/java/com/tencoding/blog/api/UserApiController.java
@@ -3,11 +3,9 @@ package com.tencoding.blog.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tencoding.blog.dto.ResponseDto;
-import com.tencoding.blog.model.RoleType;
 import com.tencoding.blog.model.User;
 import com.tencoding.blog.service.UserService;
 
